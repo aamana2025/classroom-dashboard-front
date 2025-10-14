965b5da509f8fd1697876e13c1dcfc9cdb804223
fares mohamed <fm883254@gmail.com>
2025-10-14 16:14:34 +0300

    fix backend url

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -1,2 +1,3 @@
 // export const API_URL = "https://class-room-backend-nodejs.vercel.app/api"; 
-export const API_URL = process.env.NEXT_BASE_URL_BACKEND; 
+// export const API_URL = process.env.NEXT_BASE_URL_BACKEND;
+export const API_URL = 'https://aamana-classroom-backend.vercel.app/api';
