9bce2d77d532432c4f7264b279c2acf496cefd9d
fares mohamed <fm883254@gmail.com>
2025-10-14 15:36:06 +0300

    handle baseurl

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://class-room-backend-nodejs.vercel.app/api"; 
+// export const API_URL = "https://class-room-backend-nodejs.vercel.app/api"; 
+export const API_URL = process.env.NEXT_BASE_URL_BACKEND; 
