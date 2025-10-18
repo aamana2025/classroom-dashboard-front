6a8c6be7dd1073c3ac0778bea0b662c7b27b3d82
fares mohamed <fm883254@gmail.com>
2025-10-18 15:18:36 +0300

    add google verfictaion

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,11 +12,15 @@ const cairo = Cairo({
 export const metadata = {
   title: "تطبيق الدخول",
   description: "واجهة تسجيل الدخول بالعربية",
+  verification: {
+    google: "YuczLlYL3EL3OL73luFStTsksell36C_52OGnzDQxpE", // ✅ Google site verification tag
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
+      <meta name="google-site-verification" content="YuczLlYL3EL3OL73luFStTsksell36C_52OGnzDQxpE" />
       <body className={`${cairo.variable} font-sans antialiased w-screen h-screen`}>
         <AppProvider>
           {/* Google Identity Services script */}
